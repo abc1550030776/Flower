4b0fc894d1b4e7070b871e2263e8b49491bdddcf
abc1550030776 <1550030776@qq.com>
2020-11-27 15:39:11 +0800

    添加测试的东西

--- Flower/main.cpp
+++ Flower/main.cpp
@@ -4,10 +4,18 @@
 
 int main()
 {
-	BuildDstIndex("test");
+	if(!BuildDstIndex("test"))
+	{
+		printf("build index fail\n");
+		return 1;
+	}
 
 	std::set<unsigned long long> result;
-	SearchFile("test", "word", 4, &result);
+	if(!SearchFile("test", "regulators", 10, &result))
+	{
+		printf("search fail \n");
+		return 1;
+	}
 
 	//打开文件看看查找的字符串对不对。
 	FILE* file = fopen("test", "rb");
@@ -17,21 +25,22 @@ int main()
 		return 1;
 	}
 
-	char buffer[5];
+	char buffer[11];
 
 	for (auto& val : result)
 	{
 		fpos_t pos;
 		pos.__pos = val;
 		fsetpos(file, &pos);
-		if (fread(buffer, 4, 1, file) != 1)
+		if (fread(buffer, 10, 1, file) != 1)
 		{
 			fclose(file);
 			printf("read file error filepos %llu", val);
 			return 1;
 		}
 
-		buffer[5] = '\0';
+		printf("file content %s", buffer);
+		buffer[10] = '\0';
 		if (strcmp(buffer, "word"))
 		{
 			fclose(file);
